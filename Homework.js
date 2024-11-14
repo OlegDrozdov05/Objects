ebb3b872adf76f2da48c60fdfdc64b2004e582a6
OlegDrozdov05 <skullonamotorcycle@gmail.com>
2024-11-14 08:36:51 +0300

    дз

---
+++ Homework.js
@@ -0,0 +1,83 @@
+
+// 1. Был нарушен принцип инверсии зависимостей. Исправленный код:
+
+class Service {
+    sendMessage(message) {
+        throw new Error("ты используешь интерфейс!!!");
+    }
+}
+
+class EmailService extends Service{
+    sendMessage(message) {
+        console.log(`Отправка email с сообщением: ${message}`);
+    }
+}
+
+class Notification {
+  constructor(service) {
+      this.service = service;
+  }
+ 
+  notify(message) {
+      this.service.sendMessage(message);
+  }
+}
+ 
+ // Использование
+const notification = new Notification(new EmailService); 
+notification.notify("Важное сообщение");
+ 
+ 
+ //2. Был нарушен принцип подстановки Барбары Лисков. Исправленный код: 
+
+class Bird {
+    fly() {
+        console.log("Птица летит");
+    }
+    action() {
+        this.fly();
+    }
+}
+
+class Duck extends Bird {}
+class Penguin extends Bird {
+
+    action() {
+        this.swim();
+    }
+    swim() {
+        console.log("Птица плавает")
+    }
+    fly() {
+        throw new Error("Пингвины не умеют летать");
+    }
+
+}
+ // Использование
+const birds = [new Duck(), new Penguin()];
+birds.forEach(bird => bird.action());
+ 
+
+ // 3. Был нарушен принцип единственной ответственности. Исправленный код.
+
+class Database {
+  save(user) {
+      console.log(`Сохранение пользователя ${user.getName()} в базу данных`);
+  }
+}
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+ 
+    getName() {
+        return this.name;
+    }
+}
+
+ // Использование
+
+const user = new User("Алексей", 30);
+const database = new Database;
+database.save(user);
